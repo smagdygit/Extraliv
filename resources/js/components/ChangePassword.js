4b14f27955d9a4ecb50bf0fe8e59b1b31f29f0e2
Rockyfelle <rockyfelle@live.se>
2021-11-08 09:36:51 +0100

    Lowered password length to 16

--- resources/js/components/ChangePassword.js
+++ resources/js/components/ChangePassword.js
@@ -12,7 +12,7 @@ function Clients() {
 
 	function sendNewPassword() {
 		setError('');
-		if (newUserPass.length >= 20) {
+		if (newUserPass.length >= 16) {
 			setSending(true);
 			fetch('/api/user/change-password', {
 				method: 'PUT',
@@ -50,12 +50,12 @@ function Clients() {
 				<h3>Skapa ett nytt lösenord</h3>
 				<div className="text-left">
 					<h4>Krav:</h4>
-					<p>Minst 20 karaktärer långt<br />Du får inte välja ett lösenord du har på någon annan webbplats<br />Välj något säkert, våra kunders integritet står på spel<br /><br /></p>
+					<p>Minst 16 karaktärer långt<br />Du får inte välja ett lösenord du har på någon annan webbplats<br />Välj något säkert, våra kunders integritet står på spel<br /><br /></p>
 					<h4 className="text-danger">Inte Bra:</h4>
 					<p>aaaabbbbcccc<br />1234...</p>
 					<h4 className="text-success">Bra:</h4>
 					<p>AllaBananerHeterPetrus<br />JagÄlskarAttStädaHosPeterPan<br /><br />(Dessa är endast exempel, välj inte dessa exakta lösenord)</p>
-					<h4 className={newUserPass.length < 20 ? "text-danger" : "text-success"}>{newUserPass.length} / 20{error}</h4>
+					<h4 className={newUserPass.length < 16 ? "text-danger" : "text-success"}>{newUserPass.length} / 16{error}</h4>
 					<Input
 						type='password'
 						className="mb-3"
